ffc5d4e117b6038a4df736dfdc274219dcbe070c
Dan Gartner <dgartner@gmail.com>
2015-05-14 14:10:19 -0500

    Implemented ToCommaString

---
+++ VSO.Cortana.UnitTests/UtilTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using VSO.Cortana.Service.Util;
+
+namespace VSO.Cortana.UnitTests
+{
+    [TestClass]
+    public class UtilTests
+    {
+        [TestMethod]
+        public void Test_IntArrayToDelimitedString()
+        {
+            int[] nums = { 34, 45, 56 };
+            var result = nums.ToCommaString();
+            Assert.AreEqual("34,45,56", result);
+        }
+    }
+}
